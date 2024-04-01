05c68f5b0fc63a8261448c5dff4a1a3db4c8126a
miniminitoptop <miniminitoptop@gmail.com>
2024-04-02 02:10:50 +0900

    1859, 1244 문제 추가

--- 1244/1244.py
+++ 1244/1244.py
@@ -1,44 +1,35 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+def dfs(n):
+    global ans
+    if n == N:
+        ans = max(ans,int("".join(map(str,lst))))
+        return
+
+    for i in range(L-1):
+        print(i)
+        for j in range (i+1,L):
+            print(j)
+            lst[i], lst[j] = lst[j], lst[i]
+            dfs(n+1)
+            lst[j], lst[j] = lst[i], lst[j]
+
+
 T = int(input())
 
 for test_case in range(1, T + 1):
-    M,N = map(str, input().split())
+    M, N = input().split()
     N = int(N)
-    lst2 = list(map(int,str(M)))
-    lst2.reverse()
-   
-    tm = 0
-    shin = 0
-    
-    lst_len = len(lst2)
-    
-    if N != 0 :
-        for i in range(lst_len-1,-1,-1):
-                max_num = max(lst2[0:i+1])
-                max_idx = lst2.index(max_num)
-                
-                if lst2[i] < lst2[max_idx]:
-                    mnt = lst2[i]
-                    lst2[i] = lst2[max_idx]
-                    lst2[max_idx] = mnt
-                    tm +=1
-                    if tm == N:
-                        break
-                               
-        if tm<N and (N-tm)%2 != 0:  
-            for i in lst2 :
-                if lst2.count(i) > 1:
-                    shin = 1
-                    break
-            if shin == 0:
-                mnt = lst2[1]
-                lst2[1] = lst2[0]
-                lst2[0] = mnt
-    
-    lst2.reverse()
-    sang = ''.join(str(i) for i in lst2)
-    print(f'#{test_case} {sang}')
-    
+    lst = list(map(int, M))
+    print(lst)
+
+    L = len(lst)
+    print(L)
+
+    ans = 0
+    dfs(0)
+    print(f'#{test_case} {ans}')
+
+
     
